c1e7e6f9fce35420aa3b47d0e6b09d2c541c127c
Jonathan Alvarez <j@jonalvarezz.com>
2021-06-02 12:05:03 +0200

    Move mdx to docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: [
-    '../stories/**/*.stories.mdx',
+    '../docs/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
